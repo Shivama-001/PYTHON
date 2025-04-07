90d73f66f77c8751ceffc181fe049f0fcf78614c
shivam <shivam31804@gmail.com>
2025-04-07 20:40:19 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,74 @@
+
+
+
+# print("shivam")
+# print(5 + 5 * 5 / 2)
+
+#print("shivam kumar")
+#print("shivam","kumar")
+    
+#print(,,,)
+#name = "shivam"
+#age  = 12
+#price  = 12.12
+#print(name,age,price)
+#age1 = age 
+#print(age1)
+        
+#print(type(name))
+#print(type(age))
+#print(type(price));
+#a = True
+#print(a,type(a))
+
+#a = bool;
+#print(not a)
+#b = a + "shivam" 
+#print(b)
+
+#a = float("32")
+#b = 12
+#b = str(b)
+#print(type(b))
+#print(a+b)
+
+#l = input("enter length")
+#w =  input("enter width")
+#print(l * w)
+#res  = l*w
+#print(res)
+a = int(input("enter a number"))
+b = int(input("enter secod num"))
+print(type(a),type(b),a+b,a-b)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
