d347be6b01e12ba23eaa79fdf3b6e60688fd45d7
shivam <shivam31804@gmail.com>
2025-04-09 13:24:34 +0530

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,64 @@
+#DATA TYPES AND VARIABLES IN PYTHON
+# 1. Numeric Types
+# 2. String Types
+# 3. List Types
+# 4. Tuple Types
+# 5. Set Types
+# 6. Dictionary Types
+# 7. Boolean Types
+
+#a =  "12"
+#print(int(a))
+
+# a = -5;
+# b = abs(a)
+# print(b)
+
+# = "shivam kumar"
+#print(len(a));
+#a[0] = "z"
+#print(a);
+#print(int(false))
+a= "12.12"
+a  = int(a)
+#print(int(a))
+print(type(a))
+#print(type(a))
+
+
+# import math
+
+# print(math.pi)
+
+
+# b = 12;
+# c = 12.123e-2;
+# a[0] = "a"
+# a[1] = "1"
+# print(a)
+ 
+#print(type(a),type(b),type(c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
