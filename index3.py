16c56264451e8411af0dcb1f805ba43df16007ce
shivam <shivam31804@gmail.com>
2025-04-18 12:41:47 +0530

    Add files via upload

---
+++ index3.py
@@ -0,0 +1,87 @@
+#Strings comments and conditionals in python
+
+
+#print(str1)
+# str1 = "Hello World"    
+# str2 = "hi dude"
+# print(str1 + str2)
+
+# print(len(str1 + str2))
+# print(str1[1:4])
+# print(str1[ : ])
+
+
+#print(f"value of pi : {pi}")
+
+# pi = 3.141516
+# print(f"value of pi : {pi}")
+
+# print(f"value of pi : {pi: .2f}")
+
+
+
+#types of print function in python
+
+#first is
+# print("hello world")
+# #second is
+# a = "shivam kumar"
+# b = 121212121
+# print(f"first is {a} and second is {b}")
+# #third is
+# a = "shivam kumar"
+# b = 12.12
+# print("my name is {} and age iis {}".format(a,b))
+
+#complex number in python
+
+# a = 3 + 5j
+# print(a)
+# print(type(a))
+# print(a.real)
+# print(a.imag)
+# print(a.conjugate())
+# print(a + 1)
+# print(a - 1)
+# print(a * 2)
+# print(a / 2)
+# print(a ** 2)
+# print(a ** 3)
+
+# if(condition) :
+# Statement1
+# elif(condition):
+# Statement2
+# else:
+# St
+# A
+
+
+a = int(input("Enter your age: "))
+if(a> 18):
+    print("You are eligible to vote")
+elif(a>12):
+    print("You are eligible to vote in school")
+else:
+     print("You are not eligible to vote")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
